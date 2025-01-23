d58a123db25ff2f9866f6065823807d09664afc5
NityamZWT <nityam@zealousweb.com>
2025-01-23 15:33:22 +0530

    day02 done

--- Exercises/day02/dynamicRoutes.js
+++ Exercises/day02/dynamicRoutes.js
@@ -4,6 +4,7 @@ const http = require('node:http');
 const fs = require('node:fs');
 
 const { createServer } = require('node:http');
+const { log } = require('node:console');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -12,9 +13,12 @@ const server = createServer((req, res) => {
 
     const parsedUrl = url.parse(req.url, true);
     const method = req.method;
-    const path = parsedUrl.pathname;
+    const pathname = parsedUrl.pathname;
+    // console.log("path:", pathname);
+
     const query = parsedUrl.query;
-    console.log(query);
+    const filePath = path.join('./files', query.name ?? '')
+    // console.log(query);
 
     if (method === 'GET' && path === '/') {
         res.writeHead(200);
@@ -23,93 +27,182 @@ const server = createServer((req, res) => {
         res.write('/file?name=filename : for querying particular file content \n')
         res.end();
     }
-    else if (method === 'GET' && path === '/list') {
-        fs.readdir('./files', 'utf8', (err, list) => {
-            if (err) {
-                res.writeHead(500);
-                res.write('server error!!!');
-                console.error(err);
-                return;
-            }
-            console.log(list);
-            // const files = JSON.stringify(list);
-            // console.log(files);
-            
-            res.writeHead(200,{ 'Content-Type': 'text/plain' });
-            list.forEach(element => {
-                res.write(element);
-                res.write('\n \n');
+    else if (method === 'GET' && pathname === '/list') {
+        try {
+            fs.readdir(filePath, { withFileTypes: true }, (err, list) => {
+                if (err) {
+                    console.log(err);
+                }
+                res.writeHead(200, { 'Content-Type': 'text/plain' });
+                list.forEach(element => {
+                    console.log(element.isDirectory());
+
+                    if (!element.isDirectory()) {
+                        res.write(element.name);
+                        res.write('\n \n');
+                        // res.end();
+                    }
+                });
+                res.end();
             });
-            res.end();
-        });
-    }
-    else if (method === 'GET' && path === '/file') {
-        fs.readFile(`./files/${query.name}`, 'utf8', (err, data) => {
-            if (err) {
-                res.writeHead(500);
-                res.write('server error!!!');
-                console.error(err);
-                return;
-            }
-            res.writeHead(200, { 'Content-Type': 'text/plain' });
-            res.end(JSON.stringify(data));
-            console.log(data);
-        });
-        console.log(query.name);
 
+        } catch (err) {
+            res.writeHead(500);
+            res.write('server error!!!');
+            res.end()
+            console.error(err);
+            return;
+        }
     }
-    else if (method === 'GET' && path === '/create') {
-        const content = query.content;
-        fs.writeFile(`./files/${query.name}`, content, (err, data) => {
-            if (err) {
-                res.writeHead(500);
-                res.end('server error!!!');
-                console.error(err);
-                return;
-            }
-            res.writeHead(200, { 'Content-Type': 'text/plain' });
-            res.end("file sucsessfully created");
-        });
-        console.log(query.name);
+    else if (method === 'GET' && pathname === '/file') {
+        // console.log(pathname);
 
-    }
-    else if (method === 'GET' && path === '/append') {
-        const content = query.content;
-        fs.appendFile(`./files/${query.name}`, content, err => {
-            if (err) {
-                res.writeHead(500);
-                res.end('server error!!!');
-                console.error(err);
+        try {
+            if (query.name === undefined) {
+                console.log(query.name);
+
+                res.write("please pass file name you want to read with file extention");
+                res.end();
+            }
+            else if (!fs.existsSync(filePath)) {
+                res.write("such file is not exist in directory");
+                res.end();
             } else {
-                res.writeHead(200, { 'Content-Type': 'text/plain' });
-                res.end("content sucsessfully append");
-            }
-        });
+                fs.readFile(filePath, 'utf8', (err, data) => {
 
-        console.log(query.name);
+                    if (err) {
+                        res.writeHead(500);
+                        console.log(err);
+                    }
+                    res.writeHead(200, { 'Content-Type': 'text/plain' });
+                    res.end(JSON.stringify(data));
+                    console.log(data);
+                });
+            }
+        } catch (error) {
+            res.writeHead(500);
+            res.write('server request error!!!');
+            console.error(error);
+            return;
+        }
+        // console.log(query.name);
+    }
+    else if (method === 'GET' && pathname === '/create') {
+        try {
+            const content = `${query.content} \n`;
+            const override = query.override;
+            // console.log(content);
+            if (query.name === undefined) {
+                console.log(query.name);
+
+                res.write("please pass file name you want to read with file extention");
+                res.end();
+            }
+            else if (path.extname(filePath) != '.txt') {
+                res.write("onlt .txt file are allowed to create!")
+                res.end();
+                console.log(path.extname(filePath));
+            }
+            else if (!fs.existsSync(filePath)) {
+                fs.writeFile(filePath, content, (err, data) => {
+                    if (err) {
+                        res.writeHead(500);
+                        res.end('server error!!!');
+                        console.error(err);
+                        return;
+                    }
+                    res.writeHead(200, { 'Content-Type': 'text/plain' });
+                    res.end("file sucsessfully created");
+                });
+            }
+            else if (override == 1) {
+                res.writeHead(201, { 'Content-Type': 'text/plain' });
+                res.end("file sucsessfully overide!!");
+            }
+            else {
+                res.write("file with same name already exists.\n\n if you want to append use /append?name=filname&content=newcontent\n\n if you want to override file pass override argument in query(/create?name=filename&content=newcontent&override=1)")
+                res.end()
+                // console.log("ending");
+            }
+
+            // console.log(query.name);
+        } catch (error) {
+            res.writeHead(500);
+            res.write('server request error!!!');
+            console.error(error);
+        }
+    }
+    else if (method === 'GET' && pathname === '/append') {
+        try {
+            const content = `${query.content} \n`;
+            const override = query.override;
+            // console.log(content);
+            if (query.name === undefined) {
+                console.log(query.name);
+
+                res.write("please pass file name you want to read with file extention");
+                res.end();
+            }
+            else if (path.extname(filePath) != '.txt') {
+                res.write("onlt .txt file are allowed to create!")
+                res.end();
+                console.log(path.extname(filePath));
+            }
+            else if (!fs.existsSync(filePath)) {
+                res.write('file not exist! if you want to create use /create?name=filename&content=newcontent')
+                res.end();
+            }
+
+            else {
+                fs.writeFile(filePath, content, (err, data) => {
+                    if (err) {
+                        res.writeHead(500);
+                        res.end('server error!!!');
+                        console.error(err);
+                        return;
+                    }
+                    res.writeHead(200, { 'Content-Type': 'text/plain' });
+                    res.end("file sucsessfully created");
+                });
+            }
+
+            // console.log(query.name);
+        } catch (error) {
+            res.writeHead(500);
+            res.write('server request error!!!');
+            console.error(error);
+        }
 
     }
-    else if (method === 'GET' && path === '/delete') {
-        console.log(query.name);
-
-        fs.unlink(`./files/${query.name}`, (err) => {
-            if (err) {
-                res.writeHead(500);
-                res.end('server error!!!');
-                console.error(err);
-            } else {
-                res.writeHead(200, { 'Content-Type': 'text/plain' });
-                res.end("content sucsessfully deletd!");
-                console.log('File deleted successfully!');
+    else if (method === 'GET' && pathname === '/delete') {
+        try {
+            if (!fs.existsSync(filePath)) {
+                res.write("file doesn't exist!")
             }
-        });
+            else {
+                fs.unlink(filePath, (err) => {
+                    if (err) {
+                        res.writeHead(500);
+                        res.end('server error!!!');
+                        console.error(err);
+                    }
+                    res.writeHead(200, { 'Content-Type': 'text/plain' });
+                    res.end("file sucsessfully deletd!");
+                    console.log('File deleted successfully!');
+                });
+            }
+        } catch (error) {
+            res.writeHead(500);
+            res.write('server request error!!!');
+            console.error(error);
+        }
     }
-    else{
+    else {
         res.writeHead(200);
         res.write('no such route exist!!');
         res.end()
         console.log("not existing route!!");
-        
+
     }
 });
 console.log("started");
